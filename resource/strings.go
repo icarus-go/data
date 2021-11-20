a63f32a8debd729594189e5338a1397d286ae5a4
Kevin-CC <kevinicarus@163.com>
2021-11-20 22:05:42 +0800

    feat:
    - 支持 strings, json, jsonmap 等数据结构ø

---
+++ resource/strings.go
@@ -0,0 +1,147 @@
+package resource
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"pmo-test4.yz-intelligence.com/kit/data/json"
+	"strings"
+)
+
+// Strings 根据元素组成的字符串
+type Strings []string
+
+//NewStrings
+//  Author: Kevin·CC
+//  Description: 创建字符串组,以逗号切割,拼接
+//  Param val 字符串数组
+//  Return *Strings 字符串组
+func NewStrings(val ...string) *Strings {
+	s := &Strings{}
+
+	*s = val
+
+	return s
+}
+
+func (s Strings) IndexOf(subString string) int {
+	return strings.Index(s.String(), subString)
+}
+
+//In
+//  Author: Kevin·CC
+//  Description: 是否包含元素
+//  Param val 要包含的元素
+//  Return bool 是否全部包含
+func (s Strings) In(val ...string) bool {
+	m := make(map[string]struct{}, len(s))
+	for _, val := range s {
+		m[val] = struct{}{}
+	}
+
+	for _, item := range val {
+		if _, ok := m[item]; ok {
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+func (s Strings) OrIn(val ...string) (string, bool) {
+	m := make(map[string]struct{}, len(s))
+	for _, val := range s {
+		m[val] = struct{}{}
+	}
+
+	for _, item := range val {
+		if _, ok := m[item]; ok {
+			return item, true
+		}
+	}
+
+	return "", false
+}
+
+//NotIn
+//  Author: Kevin·CC
+//  Description: 是否不包含元素
+//  Param val
+//  Return bool
+func (s Strings) NotIn(val ...string) bool {
+	m := make(map[string]struct{}, len(s))
+	for _, val := range s {
+		m[val] = struct{}{}
+	}
+
+	for _, item := range val {
+		if _, ok := m[item]; !ok {
+			return true
+		}
+	}
+
+	return false
+}
+
+// TODO 待支持 差集 , 合集 , 并集
+
+// String
+//  Description: 字符串值
+//  Author: Kevin·CC
+//  return string
+func (s Strings) String() string {
+	if len(s) == 0 {
+		return ""
+	}
+	return strings.Join(s, ",")
+}
+
+func (s *Strings) trim() *Strings {
+	if len(*s) < 1 {
+		return s
+	}
+
+	for i := 0; i < len(*s); i++ {
+		(*s)[i] = strings.TrimSpace((*s)[i])
+	}
+	return s
+}
+
+// Scan 扫描
+// Author Kevin·CC
+func (s *Strings) Scan(value interface{}) error {
+	if val, ok := value.(string); ok {
+		val := strings.ReplaceAll(val, "\"", "")
+		*s = strings.Split(val, ",")
+	}
+	return nil
+}
+
+// Value 值
+// Author Kevin·CC
+func (s *Strings) Value() (driver.Value, error) {
+	return driver.Value(s.trim().String()), nil
+}
+
+// MarshalJSON 序列化
+// Author Kevin·CC
+func (s *Strings) MarshalJSON() ([]byte, error) {
+	_ = s.trim()
+	marshal, err := json.Marshal(*s)
+	return []byte(fmt.Sprintf(`%v`, string(marshal))), err
+}
+
+// UnmarshalJSON 反序列化
+// Author Kevin·CC
+func (s *Strings) UnmarshalJSON(bytes []byte) error {
+	val := strings.ReplaceAll(string(bytes), "\"", "")
+	val = strings.ReplaceAll(val, "[", "")
+	val = strings.ReplaceAll(val, "]", "")
+	*s = strings.Split(val, ",")
+	return nil
+}
+
+// GormDataType gorm 定义数据库字段类型
+// Author Kevin·CC
+func (s *Strings) GormDataType() string {
+	return "varchar"
+}
---
+++ resource/strings_test.go
@@ -0,0 +1,67 @@
+package resource
+
+import (
+	"pmo-test4.yz-intelligence.com/kit/data/json"
+	"testing"
+)
+
+type TempString struct {
+	Values Strings `json:"value"`
+	ID     uint64  `json:"id"`
+}
+
+func TestStrings_Marshal(t *testing.T) {
+	tempString := TempString{
+		Values: *NewStrings("a", "b", "c"),
+		ID:     1,
+	}
+
+	marshal, err := json.Marshal(tempString)
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+
+	print(string(marshal))
+}
+
+func TestStrings_Unmarshal(t *testing.T) {
+
+	tempString := TempString{}
+	content := `{"value":["1","2"],"id":1}`
+	err := json.Unmarshal([]byte(content), &tempString)
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+
+	print(tempString.Values.String())
+}
+
+func TestStrings_In(t *testing.T) {
+
+	strings := NewStrings("1", "2", "3")
+
+	if strings.In("1") {
+		print("包含1")
+	}
+
+	if strings.In("1", "3") {
+		print("包含1,3")
+	}
+
+	if strings.In("1", "3") {
+		print("包含1,3")
+	}
+
+}
+
+func TestStrings_OrIn(t *testing.T) {
+
+	strings := NewStrings("1", "2", "3")
+
+	if hit, ok := strings.OrIn("1", "4"); ok {
+		print("包含", hit)
+	}
+
+}
